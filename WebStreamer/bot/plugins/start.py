bed5df74532b703984e67c7c11976d4ad07749e6
akuiiki <Myudi422@gmail.com>
2022-12-16 19:58:55 +0700

    d

---
+++ WebStreamer/bot/plugins/start.py
@@ -0,0 +1,13 @@
+# This file is a part of TG-FileStreamBot
+# Coding : Jyothis Jayanth [@EverythingSuckz]
+
+from pyrogram import filters
+from pyrogram.types import Message
+from WebStreamer.bot import StreamBot
+
+
+@StreamBot.on_message(filters.command(["start", "help"]))
+async def start(_, m: Message):
+    await m.reply(
+        f''
+    )
