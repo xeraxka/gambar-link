cda04b2a33f029cf9dc31fbe3dbb22b7b9f7cab4
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-12-30 16:22:19 +0700

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -24,7 +24,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def media_receive_handler(_, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     new_url = "https://t.me/c/1844389952/"
-    stream_link = f"{new_url}{log_msg.id}/{quote_plus(get_name(m))}"
+    stream_link = f"{new_url}{log_msg.id}"
     short_link = f"{new_url}{get_hash(log_msg)}{log_msg.id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     rm = InlineKeyboardMarkup(
