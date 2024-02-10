b7e0365cc35e6c0b07caac15d63bada2c98c7a21
akuiiki <43848105+Myudi422@users.noreply.github.com>
2024-02-10 22:02:41 +0700

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -1,5 +1,5 @@
-# Ini adalah bagian dari TG-FileStreamBot
-# Kode: Jyothis Jayanth [@EverythingSuckz]
+# This file is a part of TG-FileStreamBot
+# Coding : Jyothis Jayanth [@EverythingSuckz]
 
 import logging
 from pyrogram import filters
@@ -9,6 +9,9 @@ from WebStreamer.bot import StreamBot
 from WebStreamer.utils import get_hash, get_name
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
 
 @StreamBot.on_message(
     filters.private
@@ -21,18 +24,20 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def media_receive_handler(_, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     new_url = "https://t.me/c/1844389952/"
-    stream_link = f"{new_url}{log_msg.id} | {Var.URL}{log_msg.id}/{quote_plus(get_name(m))}"
+    stream_link = f"{new_url}{log_msg.id}"
     short_link = f"{new_url}{get_hash(log_msg)}{log_msg.id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     rm = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Buka", url=stream_link)]]
+        [[InlineKeyboardButton("Open", url=stream_link)]]
     )
     if Var.FQDN == Var.BIND_ADDRESS:
+        # dkabl
         rm = None
+    caption_text = "{} | {}{}".format(
+        stream_link, "https://subjective-tobye-myudi422.koyeb.app/", get_hash(log_msg) + log_msg.id
+    )
     await m.reply_text(
-        text="<code>{}</code>\n(<a href='{}'>Shortlink</a>)".format(
-            stream_link, short_link
-        ),
+        text=caption_text,
         quote=True,
         parse_mode=ParseMode.HTML,
         reply_markup=rm,
