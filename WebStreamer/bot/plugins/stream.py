8ec2727939f4be01e28c5eed20f8dfa5873935c0
akuiiki <43848105+Myudi422@users.noreply.github.com>
2024-02-10 21:31:35 +0700

    2 link

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -1,5 +1,5 @@
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
+# Ini adalah bagian dari TG-FileStreamBot
+# Kode: Jyothis Jayanth [@EverythingSuckz]
 
 import logging
 from pyrogram import filters
@@ -9,9 +9,6 @@ from WebStreamer.bot import StreamBot
 from WebStreamer.utils import get_hash, get_name
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
-
-
 
 @StreamBot.on_message(
     filters.private
@@ -24,21 +21,19 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def media_receive_handler(_, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     new_url = "https://t.me/c/1844389952/"
-    stream_link = f"{new_url}{log_msg.id}"
+    stream_link = f"{new_url}{log_msg.id} | {Var.URL}{log_msg.id}/{quote_plus(get_name(m))}"
     short_link = f"{new_url}{get_hash(log_msg)}{log_msg.id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     rm = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Open", url=stream_link)]]
+        [[InlineKeyboardButton("Buka", url=stream_link)]]
     )
     if Var.FQDN == Var.BIND_ADDRESS:
-        # dkabl
         rm = None
     await m.reply_text(
-        text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
+        text="<code>{}</code>\n(<a href='{}'>Shortlink</a>)".format(
             stream_link, short_link
         ),
         quote=True,
         parse_mode=ParseMode.HTML,
         reply_markup=rm,
     )
-
