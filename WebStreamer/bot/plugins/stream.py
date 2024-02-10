fa8ffe1752fec5ed853248f4119ad090510b7bfc
akuiiki <43848105+Myudi422@users.noreply.github.com>
2024-02-10 22:12:48 +0700

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -2,15 +2,13 @@
 # Coding : Jyothis Jayanth [@EverythingSuckz]
 
 import logging
-from pyrogram import filters
+from pyrogram import filters, errors
 from WebStreamer.vars import Var
 from urllib.parse import quote_plus
-from WebStreamer.bot import StreamBot
+from WebStreamer.bot import StreamBot, logger
 from WebStreamer.utils import get_hash, get_name
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
-
 
 
 @StreamBot.on_message(
@@ -18,27 +16,38 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     & (
         filters.document
         | filters.video
+        | filters.audio
+        | filters.animation
+        | filters.voice
+        | filters.video_note
+        | filters.photo
+        | filters.sticker
     ),
     group=4,
 )
 async def media_receive_handler(_, m: Message):
+    if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
+        return await m.reply("You are not <b>allowed to use</b> this", quote=True)
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-    new_url = "https://t.me/c/1844389952/"
-    stream_link = f"{new_url}{log_msg.id}"
-    short_link = f"{new_url}{get_hash(log_msg)}{log_msg.id}"
-    logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
-    rm = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Open", url=stream_link)]]
-    )
-    if Var.FQDN == Var.BIND_ADDRESS:
-        # dkabl
-        rm = None
-    caption_text = "{} | {}{}".format(
-        stream_link, "https://subjective-tobye-myudi422.koyeb.app/", get_hash(log_msg) + log_msg.id
-    )
-    await m.reply_text(
-        text=caption_text,
-        quote=True,
-        parse_mode=ParseMode.HTML,
-        reply_markup=rm,
-    )
+    stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}"
+    short_link = f"{Var.URL}{file_hash}{log_msg.id}"
+    logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+    try:
+        await m.reply_text(
+            text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
+                stream_link, short_link
+            ),
+            quote=True,
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Open", url=stream_link)]]
+            ),
+        )
+    except errors.ButtonUrlInvalid:
+        await m.reply_text(
+            text="<code>{}</code>\n\nshortened: {})".format(
+                stream_link, short_link
+            ),
+            quote=True,
+            parse_mode=ParseMode.HTML,
+        )
